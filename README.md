7a3c1cea4e336348e78f8cf7e958b5cacd3db471
Nguyenngocson101103 <127200965+Nguyenngocson101103@users.noreply.github.com>
2023-03-07 18:01:45 +0700

    Update and rename README.md to h.java

--- README.md
+++
@@ -1,1 +0,0 @@
-# NguyenNgocSon_project
---
+++ h.java
@@ -0,0 +1,6 @@
+public class  NguyenNgocSon_App {
+         public static void main(String[] args) {
+              System.out.println("Nguyễn Ngọc Sơn học Java.");
+         }
+   
+ }
